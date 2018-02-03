b360147d8f0e9e605d36ed673485c4201d7f99e3
PedroHOMota <phomota@hotmail.com>
2018-02-03 22:09:40 +0000

    Not appending number to list

--- collatz-list.rkt
+++ collatz-list.rkt
@@ -1,10 +1,17 @@
 #lang racket
 
 (define (collatz-list n)
-  (define l (list 1))
+  (define l (list ))
   (collatz-listAUX n l))
   
 (define (collatz-listAUX n l)
-   l
+  (append l n)
+  l
+  (if (= n 1) l 
+
+  (if (= (remainder n 2) 0) ; 
+       (collatz-listAUX (/ n 2) l)
+       (collatz-listAUX (+ (* 3 n) 1) l)))
   )
 
+(collatz-list 3)
