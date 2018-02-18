9fea7d1c2a4ab3575f8870f03ee5ec29d2482449
PedroHOMota <phomota@hotmail.com>
2018-02-18 11:08:04 +0000

    Cleaned code

--- rcycle.rkt
+++ rcycle.rkt
@@ -1,25 +1,4 @@
 #lang racket
-(define (rrrcycle l)
- (aux (cdr l) (car l))
-  )
-
-(define (aux l laux n)
-(if (null? (cdr laux))
-    (cons laux null)
-    (cons n n)
-    );endif 
-(if (null? (cdr l))
-    (cons (car l) laux);t
-    (aux l (car laux))
-    );endif
-  )
-
-(define (getLastN l)
-  (if (null? (cdr l))
-    (car l);t
-    (getLastN (cdr l))
-    );endif
-  );end getLastN
 
 (define (rcycle l)
   ;(cons (getLastN l) l)
