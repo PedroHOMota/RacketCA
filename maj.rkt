102991884e57bb37d866709896f9f932311ec7ec
PedroHOMota <phomota@hotmail.com>
2018-03-15 11:17:35 +0000

    Fixed return mistake

--- maj.rkt
+++ maj.rkt
@@ -1,10 +1,15 @@
 #lang racket
 
 (define (maj l1 l2 l3)
+  (if (null?  l1)
+   null
   (if (> (+ (car l1) (car l2) (car l3)) 1)
          (cons 1 (maj (cdr l1) (cdr l2) (cdr l3)))
          (cons 0 (maj (cdr l1) (cdr l2) (cdr l3)))
-         );endIF
+         ));endIF
   );endMaj
 
+
+
+(print (maj '(0 1 1) '(1 0 1) '(1 1 0)))
  
